85d02701601fd0d6b97e63f5c94895a2902f1c3e
Hai Truong <haitruong181096@gmail.com>
2018-06-26 15:50:28 +0300

    Implement PureComponent for InputField

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -1,33 +1,38 @@
-import React from "react";
+import React, { PureComponent } from "react";
 
 import "./index.css";
 
-const InputField = ({
-  id,
-  placeholder,
-  value,
-  handleClickStatus,
-  handleChange,
-  handleValidEffect,
-  userClick,
-  error
-}) => {
-  const errorEffect = handleValidEffect && handleValidEffect(userClick, error);
-  const errorMsg = errorEffect === "valid" ? "" : error;
+class InputField extends PureComponent {
+  render() {
+    const {
+      id,
+      placeholder,
+      value,
+      handleClickStatus,
+      handleChange,
+      handleValidEffect,
+      userClick,
+      error
+    } = this.props;
 
-  return (
-    <div className="input-field">
-      <input
-        id={id}
-        placeholder={placeholder}
-        value={value}
-        onBlur={handleClickStatus && handleClickStatus}
-        onChange={handleChange}
-        className={errorEffect}
-      />
-      <div className="error-msg">{errorMsg}</div>
-    </div>
-  );
-};
+    const errorEffect =
+      handleValidEffect && handleValidEffect(userClick, error);
+    const errorMsg = errorEffect === "valid" ? "" : error;
+
+    return (
+      <div className="input-field">
+        <input
+          id={id}
+          placeholder={placeholder}
+          value={value}
+          onBlur={handleClickStatus && handleClickStatus}
+          onChange={handleChange}
+          className={errorEffect}
+        />
+        <div className="error-msg">{errorMsg}</div>
+      </div>
+    );
+  }
+}
 
 export default InputField;
